7d7cce3c76d88797c2f0f91e1d369def4fd27ca4
Abhijith <abhijithpilakka@gmail.com>
2023-08-14 19:16:28 +0530

    troubleshooted the app

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-import pandas as pd
 from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData,PredictPipeline
 
@@ -40,4 +42,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")     
+    app.run(host="0.0.0.0")  
